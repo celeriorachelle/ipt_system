4e4d12fe8dda59fb7460a87ea6dc24421aca86de
abigailcajuban <202201535@cityofmalabonuniversity.edu.ph>
2025-03-16 14:10:44 +0800

    newchanges

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var registerRouter = require('./routes/register');
 var loginRouter = require('./routes/login');
+var labRouter = require('./routes/lab');
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/register', registerRouter);
 app.use('/login', loginRouter);
+app.use('/lab', labRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
